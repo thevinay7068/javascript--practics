1a2d6d53b7e5c517e37f6f6e6f298546e529516a
thevinay7068 <152369266+thevinay7068@users.noreply.github.com>
2026-07-29 13:18:56 +0000

    srting is done

--- 01-basics/01_variable.js
+++ 01-basics/01_variable.js
@@ -5,3 +5,6 @@ console.log(accountId)
 console.table([accounEmail,accounPassward,accountId])
 //prefer not to use var variable
 //because of issue in the block score and functional soope
+console.log(1+2+"3")
+console.log("1"+2+3)
+console.log("1"+"2"+3)
---
+++ 01-basics/02_string.js
@@ -0,0 +1,17 @@
+// how to intialize the string
+const gameName = String('vinaySahani')
+console.log(gameName)
+console.log(gameName[1])
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('h'))
+console.log(gameName.indexOf('a'))
+console.log(gameName.slice(0,3))
+console.log(gameName.includes('Sahani'))
+console.log(gameName.replace('S','s'))
+console.log(gameName.split(5))
+
+
+const name = "vinay sahani"
+const repoCount = 50
+console.log(`Hello my name is ${name} amd my repo on github is ${repoCount} .`)

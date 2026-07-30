28d137806c18e908d264a7657f8af194dd5939c1
thevinay7068 <152369266+thevinay7068@users.noreply.github.com>
2026-07-30 11:47:22 +0000

    object is done

---
+++ 02-basics/02_object.js
@@ -0,0 +1,10 @@
+// object literals
+const JsUser =  {
+    name  : "vinay" ,
+    age : 21 ,
+    location : "bengluru",
+    lastLoggingDays : ["monday" , "saturday"]
+     
+}
+console.log(JsUser.name);
+console.log(JsUser["age"]);

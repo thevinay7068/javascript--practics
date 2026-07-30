b2d8b9b990dae36bc72b3d66d3bb77123054e1c3
thevinay7068 <152369266+thevinay7068@users.noreply.github.com>
2026-07-30 13:20:16 +0000

    done

---
+++ 03-basics/01_function.js
@@ -0,0 +1,24 @@
+function logingUserName(userName){
+    if(!userName){
+        console.log("Please enter the userName ");
+        return
+    }
+    return `${userName} just logged in`
+ }
+      console.log(logingUserName("vinay"))  // output => vinay just logged in
+      console.log(logingUserName())        // output => Please enter the userName 
+                                            //undefined
+
+
+
+    // use of 3 dot like (...)                          
+    // basically it use to store multiple input which is not define by programmer
+      function calulateCartPrice(...num1){
+        return num1
+      }          
+      console.log(calulateCartPrice(20,30.50,50.580))      // output => [ 20, 30.5, 50.58 ]
+
+
+
+
+      
--- 03-basics/01_functon.js
+++
@@ -1,9 +0,0 @@
-function logingUserName(userName){
-    if(!userName){
-        console.log("Please enter the userName ");
-        return
-    }
-    return `${userName} just logged in`
- }
-      console.log(logingUserName("vinay"))
-      console.log(logingUserName())

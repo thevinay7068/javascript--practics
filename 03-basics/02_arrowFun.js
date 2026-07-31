6af0a03bf69a6da5e4b66ddcffbbb202bcd55204
thevinay7068 <152369266+thevinay7068@users.noreply.github.com>
2026-07-31 10:21:17 +0000

    arrowFunction is done

---
+++ 03-basics/02_arrowFun.js
@@ -0,0 +1,25 @@
+// const chai = () =>{
+//     let username = "vinay"
+//     console.log(this)
+// }
+// chai()
+// Arrow fuction 
+//const addTwo = (num1,num2) => {
+    //return num1+num2
+//}
+//console.log(addTwo(4,5))   // output => 9
+//implicit return
+// const addTwo = (num1,num2) => (num1+num2)
+     
+
+// console.log(addTwo(4,5)) // 9
+
+
+
+
+
+/// immediately Invoke function expression (IIFE)
+(function chai(){
+    //IIFE named
+    console.log(`DB connected`)
+})(); //only here used semicolan (;) otherwise not run the code
